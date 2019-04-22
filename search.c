59e71459eebfb482c13ce2f397dbefd4fab733f7
kryptiksage <kryptiksage@gmail.com>
2019-04-22 18:07:31 +0530

    added search()

--- search.c
+++ search.c
@@ -10,11 +10,25 @@ int* randomarray(int n, int max)
         a[i] = rand()%max;
     return a;
 }
-//
-void main()
+//search()
+int search(int i, int n, int *a)
+{
+    int p = -1;
+    for(int j=0; j<n; j++)
+    {
+        if(a[j] == i)
+            return j;
+        else if(a[j] > i)
+            return p;
+    }
+    return p;
+}
+int main()
 {
     int *p = randomarray(5, 5);
     for(int i=0; i<5; i++)
         printf("%i\n", p[i]);
+    int a[10] = {1, 2, 3, 4, 5};
+    printf("Index: %i\n", search(7, 5, a));
     return 0;
 }
