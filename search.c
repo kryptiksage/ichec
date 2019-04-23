7730f4ecb37caf9cd3f3e27dfa6484b15afbf677
kryptiksage <kryptiksage@gmail.com>
2019-04-23 21:18:59 +0530

    median()

--- search.c
+++ search.c
@@ -47,17 +47,30 @@ int chopsearch(int i, int n, int *arr, int amin, int amax)
     }
     
 }
-//benchmark()
-//int benchmark_chop(int n, int max, s, mut)
+//median()
+int median(int n, int *arr)
+{
+    for(int i=0; i<n; i++)
+    {
+        int  c = 0;
+        for(int j=0; j<n; j++)
+            if(arr[j]<arr[i])
+                c++;
+        if(c == n/2)
+            return arr[i];
+
+    }
+}
 int main()
 {
     int *p = randomarray(5, 5);
     /*for(int i=0; i<5; i++)
         printf("%i\n", p[i]);*/
-    int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    for(int i = 1; i <= 8; i++)
+    int a[8] = {2, 8, 3, 7, 5, 6, 4, 1};
+    /*for(int i = 1; i <= 7; i++)
     { 
-        printf("Index: %i\n", chopsearch(i, 8, a, 0, 7));
-    }
+        printf("Index: %i\n", chopsearch(i, 7, a, 0, 6));
+    }*/
+    printf("Median: %i\n", median(8, a));
     return 0;
 }
